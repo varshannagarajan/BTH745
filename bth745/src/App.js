6060ebfe4c054711ffe8bc9ac3990902ceaaff4a
Varshan Nagarajan <48139461+varshannagarajan@users.noreply.github.com>
2020-07-27 23:32:46 -0400

    Footer Version 1

--- bth745/src/App.js
+++ bth745/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import Header from './Header';
+import Footer from './Footer';
 import './App.css';
 
 function App() {
@@ -21,6 +22,7 @@ function App() {
           Learn React
         </a>
       </header>
+      <Footer></Footer>
     </div>
   );
 }
