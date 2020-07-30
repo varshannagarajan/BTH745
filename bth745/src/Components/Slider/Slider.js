69755043f7bb984c211c7438ce78a96e63b8edae
Ana Garcia <agarcia-caicedo@myseneca.ca>
2020-07-30 17:48:56 -0400

    Conditional Carousel

--- bth745/src/Components/Slider/Slider.js
+++ bth745/src/Components/Slider/Slider.js
@@ -2,20 +2,57 @@ import React from 'react';
 import Carousel, { Dots } from '@brainhubeu/react-carousel';
 import { makeStyles } from "@material-ui/core/styles"
 import { slidesToShowPlugin } from '@brainhubeu/react-carousel';
+import Typography from '@material-ui/core/Typography';
+import Grid from '@material-ui/core/Grid';
 import '@brainhubeu/react-carousel/lib/style.css';
 import './Slider.css'
 
 const useStyles = makeStyles((theme) => ({
     images: {
         height: "20vh",
-    }
+    },
+    titlePurchased: {
+        textAlign: "left",
+        paddingLeft: "0.5vh",
+    },
+    price:{
+        textAlign: "right",
+        color: "#f93a3a",
+        paddingRight: "0.5vh",
+    },
   }));
+
+function Owned(props) {
+    const classes = useStyles();
+    return(
+        <>
+        <img src={props.image} className={classes.images}/>
+        <Typography variant="subtitle1"> {props.title}</Typography>
+        </>
+    )
+}
+
+function Purchase(props) {
+    const classes = useStyles();
+    return(
+        <>
+        <img src={props.image} className={classes.images}/>
+        <Grid container spacing={2}>
+            <Grid item xs>
+                <Typography noWrap variant="subtitle1" className={classes.titlePurchased}> {props.title}</Typography>
+            </Grid>
+            <Grid item xs>
+                <Typography variant="subtitle2" className={classes.price}> {props.price}</Typography>
+            </Grid>
+        </Grid>
+        </>
+    )
+}
 
 function Slider(props) {
     for (var i = 0; i < props.games.length; i++) {
         console.log(props.games[i].picture);
     }
-    const classes = useStyles();
     return (
         <>
             <Carousel
@@ -30,7 +67,11 @@ function Slider(props) {
                 ]}
                 >
                 {props.games.map(function(game, index){
-                    return <img src={game.picture} className={classes.images}/>
+                    return (
+                        <div>
+                            {game.owned ? <Owned image={game.picture} title={game.title}/> : <Purchase  image={game.picture} title={game.title} price={game.price}/>}
+                        </div>
+                    )
                 })}
             </Carousel>
         </>
