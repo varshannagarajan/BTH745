9a329559e2427dd5cb56cabeeef26e87599ec4bd
Neil <neil.ong31@gmail.com>
2020-07-31 23:24:00 -0400

    More Game Data

--- bth745/src/assets/objects/Game.js
+++ bth745/src/assets/objects/Game.js
@@ -88,6 +88,213 @@ export const Game = [
     "genre": "Shooter",
     "owned": true,
     "rating": 5
+  },
+  {
+    "title": "Clank and a Rachet",
+    "description": "Way better than Rachet and Clank",
+    "price": 29.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Shooter",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "Counter Strike: Global Offensive",
+    "description": "Buy them stickers and skins boiiiii",
+    "price": 19.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Shooter",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Blackjack",
+    "description": "The normal blackjack you know and love. HIT!",
+    "price": 9.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": true,
+    "rating": 5
+  },
+  {
+    "title": "Texas Hold'em",
+    "description": "Are you the next Phil Ivey?",
+    "price": 9.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "Gin and Rummy",
+    "description": "Get to 100 first or get roasted for life!",
+    "price": 9.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Poker: Unleashed",
+    "description": "All that lovely poker action, but truly UNLEASHED",
+    "price": 19.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Poker: Leashed",
+    "description": "All that lovely poker action, but unfortunetly leashed",
+    "price": 0,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": true,
+    "rating": 3
+  },
+  {
+    "title": "Magic, Why Are We Gathering?",
+    "description": "Blah blah black, BLACK LOTUS, blah blah",
+    "price": 39.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": false,
+    "rating": 2
+  },
+  {
+    "title": "Pokemon TCG",
+    "description": "Pika pika?",
+    "price": 9.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Cards",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Forza: 69",
+    "description": "Microsoft bootleg version of Gran Turismo",
+    "price": 34.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 1
+  },
+  {
+    "title": "Gran Tursimo 8",
+    "description": "Feel like you're actually racing. It is the single greatest racing simulator",
+    "price": 29.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": true,
+    "rating": 5
+  },
+  {
+    "title": "F1 2020",
+    "description": "Hamilton, Bottas, Verstappen, Vettel, Sainz, who wins 2020?",
+    "price": 34.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "Twsited Metal Reboot",
+    "description": "Twisting Metal and Breaking Hearts",
+    "price": 19.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "Speed is Needed",
+    "description": "Ever watch Fast and Furious? Then you'll love this",
+    "price": 59.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Nascar 2020",
+    "description": "We love country music",
+    "price": 49.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "The Gang",
+    "description": "Its like the Crew, but not as good",
+    "price": 29.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Racing",
+    "owned": false,
+    "rating": 3
+  },
+  {
+    "title": "Draw Something",
+    "description": "You like illustrations? You'll like this",
+    "price": 49.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Make Some Films",
+    "description": "It's like Final Cut, but on a budget",
+    "price": 24.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": true,
+    "rating": 4
+  },
+  {
+    "title": "Beatsmaker",
+    "description": "Are you tring to be the next OVO40? Make them fire beats, my guy",
+    "price": 79.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Sprite Creator 2",
+    "description": "Make them sprites!",
+    "price": 19.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 5
+  },
+  {
+    "title": "Software Developer Simulator",
+    "description": "Simulate being a Software Developer",
+    "price": 34.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 3
+  },
+  {
+    "title": "Rocket Simulator 2000",
+    "description": "Become a Rocket Scientist and bring humans to mars!",
+    "price": 19.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 4
+  },
+  {
+    "title": "Benchmark Dat Joint",
+    "description": "Did you spend your life's savings on a new PC? Benchmark it to make your friends feel like peasants.",
+    "price": 10.99,
+    "picture": "/images/fortnite.png",
+    "genre": "Program",
+    "owned": false,
+    "rating": 3
   }
 ]
 
