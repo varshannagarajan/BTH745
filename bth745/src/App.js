17f11748465cc106ae4c5cf78c21cb3d25e08743
Varshan Nagarajan <48139461+varshannagarajan@users.noreply.github.com>
2020-07-31 20:27:09 -0400

    Fixed Routing
    Co-authored-by: Jordan Sie <jordansie13@gmail.com>
    Co-authored-by: James Inkster <james.inkster@live.ca>
    Co-authored-by: Julian Boyko <julian.boyko@outlook.com>

--- bth745/src/App.js
+++ bth745/src/App.js
@@ -1,13 +1,46 @@
 import React from 'react';
-import Header from './Components/Header/Header';
-import Footer from './Components/Footer/Footer';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link,
+  NavLink
+} from 'react-router-dom';
 import './App.css';
-
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
+import Footer from './Components/Footer/Footer';
+import MessageFriends from './Components/Messages/MessageFriends';
 
 function App() {
   return (
     <div className="App">
-      <Header></Header>
+      <Router>
+        <nav className="header">
+          <NavLink to="/" className='logo'>IGNITE</NavLink>
+          <div className="links">
+            <NavLink to="/library" className="link" activeClassName='selected'>Library</NavLink>
+            <NavLink to="/store" className="link" activeClassName='selected'>Store</NavLink>
+            <NavLink to="/messages" className="link" activeClassName='selected'>Messages</NavLink>
+            <NavLink to="/news" className="link" activeClassName='selected'>News</NavLink>
+          </div>
+          <Link to="/profile"><AccountCircleIcon></AccountCircleIcon></Link>
+        </nav>
+
+        <Switch>
+          <Route exact path="/" />
+
+          <Route path="/library" />
+
+          <Route path="/store" />
+
+          <Route path="/messages" component={MessageFriends} />
+
+          <Route path="/news" />
+
+          <Route path="/profile" />
+
+        </Switch>
+      </Router>
       <Footer></Footer>
     </div>
   );
